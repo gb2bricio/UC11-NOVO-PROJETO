6e78fc4e0c8586b9426be66a97d669cab776652b
gb2bricio <bricioslgcerto@gmail.com>
2026-07-30 22:16:35 -0300

    teste SideBar

---
+++ pages/dashboard.php/dashboard.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>sistema de exemplo</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+</head>
+
+<body>
+  <!-- Navbar -->
+
+  <?php include("../../include/navbar.php"); ?>
+
+
+   <div class="container-fluid bg-warning">
+    <div class="row">
+      <div id= "sidebar" class="col-auto d-none d-lg-block">SideBar</div>
+      <div class="col">Principal</div>
+      <p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p>
+      <p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p>
+      <p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p>
+      <p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p>
+      <p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p>
+      <p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p>
+      <p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p>
+      <p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p>
+      <p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p>
+      <p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p>
+      <p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p>
+      <p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p>
+      <p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p>
+      <p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p><p>&puncsp;</p>
+      
+    
+
+
+    </div> 
+    
+
+
+
+
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
